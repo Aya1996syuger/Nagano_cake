020b925a4123b8b4825b1360c56e8aa07c4a8345
Aya1996syuger <ec2-user@ip-172-31-79-196.ec2.internal>
2021-11-25 11:03:55 +0000

    itemOK

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -23,6 +23,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+     @item = Item.find(params[:id])
+     if @item.update(item_params)
+        redirect_to admin_item_path(@item.id)
+     else
+       @items = Item.all
+       render :edit
+     end
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9228f85194259d930248c2d381f0d07cfad7fa32b5c3c17de5b573b9a10591044bc9c10cf78cab123a51d197a8c71d03c7cc73d832ea04b5df754d93b8024e77'
